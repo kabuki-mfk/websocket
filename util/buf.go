1679eec1ddb30f55a2766f0b8f93735e5c2faf27
kabuki-mfk <67349826+kabuki-mfk@users.noreply.github.com>
2022-09-16 14:40:58 +0800

    重构BaseBuf接口
    增加bufArray作为缓冲区的内部数据结构
    BaseBuf.Array()将返回一个bufArray

--- util/buf.go
+++ util/buf.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"errors"
 	"io"
 	"reflect"
 	"unsafe"
@@ -28,15 +29,81 @@ func (e ReaderError) Error() string {
 	return "websocket.buf:\n ReaderError: " + string(e)
 }
 
-const(
+const (
 	ErrInSufficientBytes = BufferError("insufficient bytes available in the buffer")
-	ErrReadNoEnough = BufferError("length of data read from Buffer is not enough")
+	ErrReadNoEnough      = BufferError("length of data read from Buffer is not enough")
 )
+
+type bufArray struct {
+	data []byte
+	next *bufArray
+}
+
+func _NewBufArray(arrays ...[]byte) *bufArray {
+	if len(arrays) == 1 {
+		return &bufArray{
+			data: arrays[0],
+		}
+	}
+
+	var head, tail, temp *bufArray
+	for i := 0; i < len(arrays); i++ {
+		array := arrays[i]
+
+		if i == 0 {
+			head = &bufArray{
+				data: array,
+			}
+			temp = head
+			continue
+		}
+
+		tail = &bufArray{
+			data: array,
+		}
+		temp.next = tail
+		temp = tail
+	}
+
+	return head
+}
+
+func (a *bufArray) Data() []byte {
+	return a.data
+}
+
+func (a *bufArray) Next() *bufArray {
+	return a.next
+}
+
+func findArray(head *bufArray, i int) (offset int, array *bufArray) {
+	if i < 0 {
+		panic(errors.New("index out of the bound"))
+	}
+
+	array = head
+	length := 0
+
+	for {
+		l := len(array.data)
+		if length+l > i {
+			offset = i - length
+			break
+		} else if array.Next() == nil {
+			panic(errors.New("index out of the bound"))
+		}
+
+		array = array.Next()
+		length += l
+	}
+
+	return
+}
 
 type BaseBuf interface {
 	Size() int
 
-	Array() []byte
+	Array() *bufArray
 
 	Copy() BaseBuf
 
@@ -90,14 +157,14 @@ type ByteBuf struct {
 	wIdx int
 
 	//buf
-	array []byte
+	array *bufArray
 }
 
 func NewBufWithSize(size int) BaseBuf {
 	return &ByteBuf{
 		rIdx:  0,
 		wIdx:  0,
-		array: make([]byte, size),
+		array: _NewBufArray(make([]byte, size)),
 	}
 }
 
@@ -105,7 +172,7 @@ func NewBufWithArray(b []byte) BaseBuf {
 	return &ByteBuf{
 		rIdx:  0,
 		wIdx:  0,
-		array: b,
+		array: _NewBufArray(b),
 	}
 }
 
@@ -120,10 +187,10 @@ func unsafeWriteString(s string, dstBuf BaseBuf) (n int, err error) {
 
 	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
 	bh := reflect.SliceHeader{
-        Data: sh.Data,
-        Len:  sh.Len,
-        Cap:  sh.Len,
-    }
+		Data: sh.Data,
+		Len:  sh.Len,
+		Cap:  sh.Len,
+	}
 	b := *(*[]byte)(unsafe.Pointer(&bh))
 
 	n, err = dstBuf.WriteBytes(b)
@@ -139,11 +206,138 @@ func unsafeReadString(len int, srcBuf BaseBuf) (str string, err error) {
 
 	rb := make([]byte, len)
 	_, err = srcBuf.ReadBytes(rb)
-	
+
 	str = *(*string)(unsafe.Pointer(&rb))
 	return
 }
 
+func transfor(dst BaseBuf, src BaseBuf) int {
+	return transforWithBound(dst, 0, dst.Size(), src, 0, src.Size())
+}
+
+func transforWithBound(dst BaseBuf, dFrom, dTo int, src BaseBuf, sFrom, sTo int) int {
+	dstLen, srcLen := dTo - dFrom, sTo - sFrom
+	if dstLen < 1 || srcLen < 1 {
+		return 0
+	}
+
+	dstOffset, dstFrom := findArray(dst.Array(), dFrom)
+	srcOffset, srcFrom := findArray(src.Array(), sFrom)
+	transforLen := 0
+
+	for {
+		dEnd := len(dstFrom.data)
+		sEnd := len(srcFrom.data)
+
+		if dstLen < len(dstFrom.data) {
+			dEnd = dstOffset + dstLen
+		}
+		if srcLen < len(dstFrom.data) {
+			sEnd = srcOffset + srcLen
+		}
+
+		n := copy(dstFrom.data[dstOffset:dEnd], srcFrom.data[srcOffset:sEnd])
+		dstOffset += n
+		srcOffset += n
+		dstLen -= n
+		srcLen -= n
+
+		transforLen += n
+
+		//array transfor data done
+		if dstLen == 0 || srcLen == 0 {
+			break
+		}
+
+		if dstOffset == len(dstFrom.data) && dstLen > 0 {
+			if dstFrom.next == nil {
+				panic("out of bound on transfor bufarray data")
+			}
+			dstFrom = dstFrom.next
+			dstOffset = 0
+		}
+
+		if srcOffset == len(srcFrom.data) && srcLen > 0 {
+			if srcFrom.next == nil {
+				panic("out of bound on transfor bufarray data")
+			}
+			srcFrom = srcFrom.next
+			srcOffset = 0
+		}
+	}
+
+	return transforLen
+}
+
+func bufGetBytes(srcBuf BaseBuf, i int, dst []byte) error {
+	err := checkIndex(i, srcBuf)
+	if err != nil {
+		return err
+	}
+
+	dstLen := len(dst)
+	offset, array := findArray(srcBuf.Array(), i)
+
+	l := copy(dst, array.data[offset:])
+	dstLen -= l
+
+	if dstLen > 1 && array.next != nil {
+		dOffset := l
+		array = array.next
+		for {
+			l = copy(dst[dOffset:], array.data)
+			dstLen -= l
+
+			if dstLen < 1 {
+				break
+			}
+			
+			if array.next == nil {
+				break
+			}
+
+			dOffset += l
+			array = array.next
+		}
+	}
+
+	return nil
+}
+
+func bufSetBytes(dstBuf BaseBuf, i int, src []byte) error {
+	err := checkIndex(i, dstBuf)
+	if err != nil {
+		return err
+	}
+
+	dstLen := dstBuf.Size() - i
+	offset, array := findArray(dstBuf.Array(), i)
+
+	l := copy(array.data[offset:], src)
+	dstLen -= l
+
+	if dstLen > 1 && array.next != nil {
+		sOffset := l
+		array = array.next
+		for {
+			l = copy(array.data, src[sOffset:])
+			dstLen -= l
+
+			if dstLen < 1 {
+				break
+			}
+
+			if array.next == nil {
+				break
+			}
+
+			sOffset += l
+			array = array.next
+		}
+	}
+
+	return nil
+} 
 
 func checkIndex(idx int, buf BaseBuf) error {
 	if idx < 0 {
@@ -157,32 +351,38 @@ func checkIndex(idx int, buf BaseBuf) error {
 	return nil
 }
 
-func (b *ByteBuf) ReadIndex(idx int) error {
-	err := checkIndex(idx, b)
-
-	if err != nil {
-		return err
-	}
-
-	if idx > b.wIdx {
-		return OutOfBufError("idx must be less than or equal to writeIndex")
-	}
-
-	b.rIdx = idx
-	return nil
-}
-
-func (b *ByteBuf) WriteIndex(idx int) error {
-	if idx < 0 {
+func readIndex(old *int, new int, buf BaseBuf) error {
+	if new < 0 {
 		return OutOfBufError("idx must be a positive")
 	}
 
-	if idx > b.Size() {
+	if new > buf.GetWriteIndex() {
+		return OutOfBufError("idx must be less than or equal to writeIndex")
+	}
+
+	*old = new
+	return nil
+}
+
+func writeIndex(old *int, new int, buf BaseBuf) error {
+	if new < 0 {
+		return OutOfBufError("idx must be a positive")
+	}
+
+	if new > buf.Size() {
 		return OutOfBufError("idx must be less than or equal to bufsize")
 	}
 
-	b.rIdx = idx
+	*old = new
 	return nil
+}
+
+func (b *ByteBuf) ReadIndex(idx int) error {
+	return readIndex(&b.rIdx, idx, b)
+}
+
+func (b *ByteBuf) WriteIndex(idx int) error {
+	return writeIndex(&b.wIdx, idx, b)
 }
 
 func (b *ByteBuf) GetWriteIndex() int {
@@ -194,16 +394,16 @@ func (b *ByteBuf) GetReadIndex() int {
 }
 
 func (b *ByteBuf) Size() int {
-	return len(b.array)
+	return len(b.array.data)
 }
 
-func (b *ByteBuf) Array() []byte {
+func (b *ByteBuf) Array() *bufArray {
 	return b.array
 }
 
 func (b *ByteBuf) Copy() BaseBuf {
 	cp := make([]byte, b.Size())
-	copy(cp, b.array)
+	copy(cp, b.array.data)
 	return NewBufWithArray(cp)
 }
 
@@ -221,7 +421,7 @@ func (buf *ByteBuf) GetByte(i int) (b byte, err error) {
 		return 0, err
 	}
 
-	b = buf.array[i]
+	b = buf.array.data[i]
 	return
 }
 
@@ -231,7 +431,7 @@ func (b *ByteBuf) GetBytes(i int, dst []byte) error {
 		return err
 	}
 
-	copy(dst, b.array[i:])
+	copy(dst, b.array.data[i:])
 	return nil
 }
 
@@ -241,7 +441,7 @@ func (buf *ByteBuf) SetByte(i int, b byte) error {
 		return err
 	}
 
-	buf.array[i] = b
+	buf.array.data[i] = b
 	return nil
 }
 
@@ -251,7 +451,7 @@ func (b *ByteBuf) SetBytes(i int, src []byte) error {
 		return err
 	}
 
-	copy(b.array[i:], src)
+	copy(b.array.data[i:], src)
 	return nil
 }
 
@@ -260,7 +460,7 @@ func (b *ByteBuf) DiscardReadBytes() {
 		return
 	}
 
-	copy(b.array, b.array[b.rIdx:b.wIdx])
+	copy(b.array.data, b.array.data[b.rIdx:b.wIdx])
 }
 
 func (b *ByteBuf) WriteTo(w io.Writer) error {
@@ -273,7 +473,7 @@ func (b *ByteBuf) WriteToWithLen(len int, w io.Writer) (err error) {
 		err = ErrInSufficientBytes
 	}
 
-	wb, err := w.Write(b.array[b.rIdx : b.rIdx + len])
+	wb, err := w.Write(b.array.data[b.rIdx : b.rIdx+len])
 	if wb < len && err == nil {
 		err = io.ErrShortWrite
 	}
@@ -287,7 +487,7 @@ func (buf *ByteBuf) WriteByte(b byte) error {
 		return ErrInSufficientBytes
 	}
 
-	buf.array[buf.wIdx] = b;
+	buf.array.data[buf.wIdx] = b
 	buf.wIdx += 1
 	return nil
 }
@@ -297,7 +497,7 @@ func (buf *ByteBuf) WriteBytes(b []byte) (n int, err error) {
 		return 0, ErrInSufficientBytes
 	}
 
-	wb := copy(buf.array[buf.wIdx:], b)
+	wb := copy(buf.array.data[buf.wIdx:], b)
 	if wb < len(b) {
 		err = ErrInSufficientBytes
 	}
@@ -320,7 +520,7 @@ func (b *ByteBuf) ReadFromWithLen(len int, r io.Reader) (n int, err error) {
 		return 0, ErrInSufficientBytes
 	}
 
-	rb, err := r.Read(b.array[b.wIdx : b.wIdx+len])
+	rb, err := r.Read(b.array.data[b.wIdx : b.wIdx+len])
 
 	if rb < 0 {
 		panic(ReaderError("reader returned nagative count from Read"))
@@ -336,7 +536,7 @@ func (b *ByteBuf) ReadByte() (rb byte, err error) {
 		return 0, ErrInSufficientBytes
 	}
 
-	rb = b.array[b.rIdx]
+	rb = b.array.data[b.rIdx]
 	b.rIdx += 1
 	return
 }
@@ -346,7 +546,7 @@ func (buf *ByteBuf) ReadBytes(b []byte) (n int, err error) {
 		return 0, ErrInSufficientBytes
 	}
 
-	n = copy(b, buf.array[buf.rIdx:buf.wIdx])
+	n = copy(b, buf.array.data[buf.rIdx:buf.wIdx])
 
 	if n < len(b) {
 		err = ErrReadNoEnough
